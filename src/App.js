597315cd207ae6c98c62e59bb28dff4e0a2c22e1
Juwon Lee <a8316010@gmail.com>
2023-04-24 23:47:30 +0900

    updated reducer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 //import logo from "./logo.svg";
-import { useState } from "react";
+//import { useState } from "react";
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import Box from "./component/Box";
 
 function App() {
-  const [count, setCount] = useState;
+  let count = useSelector((state) => state.count);
+  let id = useSelector((state) => state.id);
+  let password = useSelector((state) => state.password);
+  const dispatch = useDispatch();
   const increase = () => {
-    setCount(count + 1);
+    dispatch({ type: "INCREMENT", payload: { num: 5 } });
+  };
+  const decrease = () => {
+    dispatch({ type: "DECREMENT", payload: { num: 5 } });
+  };
+  const login = () => {
+    dispatch({ type: "LOGIN", payload: { id: "123", password: "123" } });
   };
   return (
     <div>
+      <h1>
+        {id},{password}
+      </h1>
       <h1>{count}</h1>
       <button onClick={increase}>증가!</button>
+      <button onClick={decrease}>감소!</button>
+      <button onClick={login}>Login</button>
+
+      <Box />
     </div>
   );
 }
